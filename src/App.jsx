a4590e0b345b4d79d29604495f8fcd7db73b418b
shinohara <taisei.shinohara1329@gmail.com>
2023-10-01 12:09:37 +0900

    feat: useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css'
 
 function App() {
@@ -9,9 +9,13 @@ function App() {
     console.log(count)
   }
 
+  useEffect(() => {
+    console.log("hello hooks!");
+  }, [count]);
+
   return (
     <div className="App">
-      <h1>UseState</h1>
+      <h1>UseState, useEffect</h1>
       <p>{count}</p>
       <button onClick={handleClick}>+</button>
     </div>
