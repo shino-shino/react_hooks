6afa311aa6e5a3e8079348a58aa3078a0464686f
shinohara <taisei.shinohara1329@gmail.com>
2023-10-01 12:04:38 +0900

    feat: useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import { useState } from 'react';
 import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count+1);
+    console.log(count)
+  }
+
   return (
     <div className="App">
-
+      <h1>UseState</h1>
+      <p>{count}</p>
+      <button onClick={handleClick}>+</button>
     </div>
   );
 }
